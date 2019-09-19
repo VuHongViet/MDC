649ef1601dfc9dcb676a093b77eb1c83306d24df
= <=>
2019-09-19 18:59:40 +0700

    draw lstv

--- ansao/ansao.php
+++ ansao/ansao.php
@@ -1,10 +1,13 @@
 <?php
-$can_nam=3;
-$chi_nam=1;
-$thang=2;
-$ngay=9;
-$gio=5;
-$gioi_tinh=1;
+
+session_start();
+
+// $can_nam=3;
+// $chi_nam=1;
+// $thang=2;
+// $ngay=9;
+// $gio=5;
+// $gioi_tinh=1;
 
 // $can_nam = $_POST["can_nam"];
 // $chi_nam = $_POST["chi_nam"];
@@ -12,6 +15,17 @@ $gioi_tinh=1;
 // $ngay = $_POST["ngay"];
 // $gio = $_POST["gio"];
 // $gioi_tinh = $_POST["gioi_tinh"];
+
+
+if (isset($_POST['submit'])) {
+	$data = json_decode($_POST['json'],true);
+	$can_nam = $data['Can_nam'];
+	$chi_nam = $data['Chi_nam'];
+	$thang =$data['Am_Lich']['thang_al'];
+	$ngay =$data['Am_Lich']['ngay_al'];
+	$gio =$data['Am_Lich']['gio_al'];
+	$gioi_tinh=$data['Gioitinh'];
+ }
 $obj_thong_tin_fake = (object)[
   	"Name" => "ansao.zz.mu",
     "Gioitinh" => 1,
@@ -56,8 +70,8 @@ function an_sao($can_nam,$chi_nam,$thang,$ngay,$gio){
 	an_phu_tinh_can($can_nam);
 	an_phu_tinh_chi($chi_nam);
 	an_phu_tinh_tng($thang,$ngay,$gio);
-	
-  echo json_encode($la_so_thien_ban);
+	  $_SESSION['data']= json_encode($la_so_thien_ban);
+	header("location:velaso.php");
 	//$b = array_column($obj_sao,null,"ten");
 	// echo json_encode(array_column($obj_sao,null,"ten"));
 
